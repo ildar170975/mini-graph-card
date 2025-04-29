e44de1bfdcf9db0c5ba06fd9c1d996f6416f9de2
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-04-29 09:34:08 +0300

    Update and rename formatNumber.js to formatNumber.ts

--- src/formatNumber.ts
+++ src/formatNumber.ts
@@ -1,52 +1,52 @@
 // adapted from HomeAssistant frontend
 
-export const NumberFormat = {
-  language: "language",
-  system: "system",
-  comma_decimal: "comma_decimal",
-  decimal_comma: "decimal_comma",
-  space_comma: "space_comma",
-  none: "none",
+export enum NumberFormat {
+  language = "language",
+  system = "system",
+  comma_decimal = "comma_decimal",
+  decimal_comma = "decimal_comma",
+  space_comma = "space_comma",
+  none = "none",
 }
 
-export const TimeFormat = {
-  language: "language",
-  system: "system",
-  am_pm: "12",
-  twenty_four: "24",
+export enum TimeFormat {
+  language = "language",
+  system = "system",
+  am_pm = "12",
+  twenty_four = "24",
 }
 
-export const TimeZone = {
-  local: "local",
-  server: "server",
+export enum TimeZone {
+  local = "local",
+  server = "server",
 }
 
-export const DateFormat = {
-  language: "language",
-  system: "system",
-  DMY: "DMY",
-  MDY: "MDY",
-  YMD: "YMD",
+export enum DateFormat {
+  language = "language",
+  system = "system",
+  DMY = "DMY",
+  MDY = "MDY",
+  YMD = "YMD",
 }
 
-export const FirstWeekday = {
-  language: "language",
-  monday: "monday",
-  tuesday: "tuesday",
-  wednesday: "wednesday",
-  thursday: "thursday",
-  friday: "friday",
-  saturday: "saturday",
-  sunday: "sunday",
+export enum FirstWeekday {
+  language = "language",
+  monday = "monday",
+  tuesday = "tuesday",
+  wednesday = "wednesday",
+  thursday = "thursday",
+  friday = "friday",
+  saturday = "saturday",
+  sunday = "sunday",
 }
 
-export const FrontendLocaleData = {
-  language: string,
-  number_format: NumberFormat,
-  time_format: TimeFormat,
-  date_format: DateFormat,
-  first_weekday: FirstWeekday,
-  time_zone: TimeZone,
+export interface FrontendLocaleData {
+  language: string;
+  number_format: NumberFormat;
+  time_format: TimeFormat;
+  date_format: DateFormat;
+  first_weekday: FirstWeekday;
+  time_zone: TimeZone;
 }
 
 const numberFormatToLocale = (
