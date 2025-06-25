888485d4e39f7721ee766a5976db78555ea1ed6f
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-06-25 06:58:26 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -681,7 +681,7 @@ class MiniGraphCard extends LitElement {
       const c2 = color_thresholds[index - 1];
       if (c2) {
         const factor = (c2.value - state) / (c2.value - c1.value);
-        intColor = interpolateRgb(c2.color, c1.color)(factor);
+        intColor = interpolateRgb(c2.color, c1.color, factor);
       } else {
         intColor = index
           ? color_thresholds[color_thresholds.length - 1].color
