fa51b3c3dade7e7d7ea9050764389bb4f8ca06e5
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-06-25 09:47:24 +0300

    Update interpolateRgb.js

--- src/interpolateRgb.js
+++ src/interpolateRgb.js
@@ -1,1 +1,11 @@
+import { interpolateRgb } from 'd3-interpolate';
 
+export default (start, end, y) => {
+  const _start = isAssumingCssVar(start)
+    ? convertCssVarToColor(start)
+    : start;
+  const _end = isAssumingCssVar(end)
+    ? convertCssVarToColor(end)
+    : end;
+  return interpolateRgb(_start, _end)(y);
+};
