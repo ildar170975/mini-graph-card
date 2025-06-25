fdc6a7017b4b3dd8c55a78d5777d1fee0dc68899
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-06-25 07:10:51 +0300

    Update interpolateRgb.js

--- src/interpolateRgb.js
+++ src/interpolateRgb.js
@@ -1,7 +1,5 @@
 import { interpolateRgb } from 'd3-interpolate';
 
-interpolateRgbExtended(start, end, y) {
+export default (start, end, y) {
   return interpolateRgb(start, end)(y);
 }
-
-export { interpolateRgbExtended as default };
