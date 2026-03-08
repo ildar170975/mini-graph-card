b5567acfdcf113ffb2b095c87b6319c3e47fa886
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-03-08 03:37:34 +0300

    Update locale.js

--- src/locale.js
+++ src/locale.js
@@ -1,7 +1,7 @@
 // fragment of format_number.ts from HA frontend converted to JS
 
 // this var was converted from TS enum
-/* must be uncommented before nerging with https://github.com/kalkih/mini-graph-card/pull/1347
+/* must be uncommented before merging with https://github.com/kalkih/mini-graph-card/pull/1347
 const TimeFormat = Object.freeze({
   language: 'language',
   system: 'system',
