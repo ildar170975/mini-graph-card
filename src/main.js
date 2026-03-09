69561c449cfd200d4e0e26c27401ff631ebfc7c4
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-03-09 07:19:44 +0300

    added description for getMinMaxLineWidth()

--- src/main.js
+++ src/main.js
@@ -100,6 +100,10 @@ class MiniGraphCard extends LitElement {
     };
   }
 
+  /**
+  * Returns min & max "line_width" values defined globally for a card & for all entities individually
+  * @returns {object} min & max "line_width" values
+  */
   getMinMaxLineWidth() {
     const arr = this.config.entities
       .map(entityConfig => entityConfig.line_width)
