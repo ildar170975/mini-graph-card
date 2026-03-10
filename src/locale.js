6ebf34624485b7d7d521a7b09a5fcdbb84dd1c38
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-03-11 02:23:00 +0300

    add blankBeforePercent()

---
+++ src/locale.js
@@ -0,0 +1,25 @@
+// This function is taken from HA Frontend
+/**
+ * Check if a whitespace is needed before a "%" unit dependently on a locale
+ * @param localeOptions The user-selected language and formatting, from `hass.locale`
+ * @returns {string} Whitespace if needed before "%", empty otherwise
+ */
+const blankBeforePercent = (
+  localeOptions, // FrontendLocaleData
+) => {
+  switch (localeOptions.language) {
+    case "cs":
+    case "de":
+    case "fi":
+    case "fr":
+    case "sk":
+    case "sv":
+      return " ";
+    default:
+      return "";
+  }
+};
+
+export {
+  blankBeforePercent,
+};
