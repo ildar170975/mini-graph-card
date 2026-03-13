15520a15c9948d6eeacb302321af124819395d4e
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-03-13 05:20:24 +0300

    correct description

--- src/locale.js
+++ src/locale.js
@@ -1,12 +1,10 @@
-// This function is taken from HA Frontend
 /**
  * Checks if a whitespace is needed before a "%" unit dependently on a locale
- * @param localeOptions Object containing a user-selected language and formatting
+ * @param {FrontendLocaleData} localeOptions Object containing
+ * a user-selected language and formatting
  * @returns {string} Whitespace if needed before "%", empty otherwise
  */
-const blankBeforePercent = (
-  localeOptions, // FrontendLocaleData
-) => {
+const blankBeforePercent = localeOptions => {
   switch (localeOptions.language) {
     case 'cs':
     case 'de':
