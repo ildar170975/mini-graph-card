17634ca08f87e81d768f043c6be668781f6ac76c
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-04-19 21:47:27 +0300

    linter

--- src/others.js
+++ src/others.js
@@ -15,7 +15,7 @@ const getFactor = (config, index = undefined) => {
   let value_factor;
   if (index >= 0 && config.entities[index] && config.entities[index].value_factor !== undefined) {
     // provided for a possible per-entity value_factor
-    value_factor = config.entities[index].value_factor;
+   { value_factor } = config.entities[index];
   } else if (index >= 0 && config.entities[index] && config.entities[index].y_axis === 'secondary'
     && config.value_factor_secondary !== undefined) {
     // use value_factor_secondary for entities with 'y_axis: secondary'
@@ -25,7 +25,7 @@ const getFactor = (config, index = undefined) => {
     value_factor = config.value_factor_secondary;
   } else {
     // use a global value_factor
-    value_factor = config.value_factor;
+   { value_factor } = config;
   }
 
   if (value_factor === undefined) {
@@ -33,9 +33,8 @@ const getFactor = (config, index = undefined) => {
     return 1;
   }
 
-  const getExponent = (factor) => 10 ** factor;
-  const logValueFactor = (value_factor) =>
-    log(`invalid value_factor: ${JSON.stringify(value_factor)}`);
+  const getExponent = factor => 10 ** factor;
+  const logValueFactor = factor_obj => log(`invalid value_factor: ${JSON.stringify(factor_obj)}`);
 
   if (typeof value_factor === 'object') {
     const { type, factor } = value_factor;
